74fc068a8f6e8619e41b4c147df471425609fcc4
Brcinko <lukas.klescinec@gmail.com>
2017-02-01 21:46:37 +0100

    added set_config Create_config_line and read_conf_file mehod into scheduler-configurator.py

--- scheduler-configurator.py
+++ scheduler-configurator.py
@@ -5,6 +5,37 @@
 """
 from settings import NOVA_CONF_FILE
 
-with open(NOVA_CONF_FILE) as f:
-    content = f.readlines()
-print content
+test_input = {
+    'filters': ['RamFilter', 'DiskFilter', 'ComputeFilter']
+}
+
+
+def set_config(config_request):
+    conf_line = create_config_line(test_input['filters'])
+    print conf_line
+    file = read_conf_file()
+    for f in file:
+        if f[:25] == 'scheduler_default_filters':
+            print f
+
+
+def create_config_line(filters):
+    config_string = 'scheduler_default_filters = '
+    i = 0
+    for f in filters:
+        if i == 0:
+            config_string += f
+        else:
+            config_string += ', '
+            config_string += f
+        i +=1
+    return config_string
+
+
+def read_conf_file():
+    with open(NOVA_CONF_FILE) as f:
+        content = f.readlines()
+    # print content
+    return content
+
+set_config("")
