3dbb11629de4210d58bcecb53bb5500f3cc516f8
Brcinko <lukas.klescinec@gmail.com>
2017-03-16 15:57:28 +0100

    added each weight into analyzer, fixed issue with

--- analyzer.py
+++ analyzer.py
@@ -6,6 +6,7 @@
 
 from settings import WEIGHTS_DICTIONARY, DEFAULT_FILTERS
 import db_connector
+import pprint
 
 test_input = {
     'filters': ['CoreFilter', 'ComputeFilter', 'RamFilter'],
@@ -24,38 +25,41 @@ test_input = {
 
 def analyze_stats(db, hosts_list):
     # get stats
-    query = '{ \'meta.doc_definition\': {$nin: True}}'
+    # query = '{ \'meta.doc_definition\': {$nin: True}}'
+    query = {}
+    query['meta.doc_definition'] = {}
+    query['meta.doc_definition']['$nin'] = [True]
     collection = db_connector.get_collection(db=db, collection_name='hosts_statistics')
     documents = db_connector.get_documents(collection=collection, query=query)
+    pprint.pprint(documents)
     response = {}
     response['filters'] = []
     response['weights'] = []
     # ------------------WEIGHT ANALYSIS------------------------
-# TODO---------------------------------------------------------------------------------
-    hosts_variance = []
-    for h in hosts_list:
-        host_stats_list = []
-        for d in documents:
-            # if stats are from this very host
-            if h == d['meta']['host_id']:
-                for s in d['stats']:
-                    # TODO .used and .average in stats are NOT the same - some distinguisher must be provide
-                    # TODO change [0] to each weight type which is used
-                    if WEIGHTS_DICTIONARY[0]['stat_name'] in s['stat_name']:
-                        # missing same unit check
-                        host_stats_list.append(s['stat_value'])
-        variance = compute_variance(stats=host_stats_list)
-        hosts_variance.append(variance)
-    multiplicator = compute_multiplicator(variances=hosts_variance)
-    # append new weight into response
-    weightx = {}
-    weightx['weight_name'] = WEIGHTS_DICTIONARY[0]['weight_name']
-    weightx['weight_value'] = multiplicator
-    response['weights'].append(weightx)
-# TODO-----------------------------------------------------------------------------------
+    for w in WEIGHTS_DICTIONARY:
+        hosts_variance = []
+        for h in hosts_list:
+            host_stats_list = []
+            for d in documents:
+                # if stats are from this very host
+                if h == d['meta']['host_id']:
+                    for s in d['stats']:
+                        # TODO .used and .average in stats are NOT the same - some distinguisher must be provide
+                        if w['stat_name'] in s['stat_name']:
+                            # missing same unit check
+                            host_stats_list.append(s['stat_value'])
+            variance = compute_variance(stats=host_stats_list)
+            hosts_variance.append(variance)
+        multiplicator = compute_multiplicator(variances=hosts_variance)
+        # append new weight into response
+        weightx = {}
+        weightx['weight_name'] = w['weight_name']
+        weightx['weight_value'] = multiplicator
+        response['weights'].append(weightx)
 
     # -------------FILTER ANALYSIS-----------------------------
     response['filters'] = DEFAULT_FILTERS
+    pprint.pprint(response)
     return test_input
 
 
